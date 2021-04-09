1b11025d019683e328cae4281ae7799cab738f1e
Anthony Candaele <info@anthonycandaele.com>
2021-04-09 10:43:16 +0200

    implement second confirmation email for webshop owner

--- src/services/email-service/order-confirmation.js
+++ src/services/email-service/order-confirmation.js
@@ -1,6 +1,6 @@
 const { formatCurrency } = require("../../lib/currency");
 const { orders } = require("../crystallize");
-const { sendEmail} = require("./utils");
+const { sendEmail } = require("./utils");
 
 module.exports = async function sendOrderConfirmation(orderId) {
   try {
@@ -9,8 +9,8 @@ module.exports = async function sendOrderConfirmation(orderId) {
 
     const { email } = order.customer.addresses[0];
 
-    const additionalInformation = JSON.parse(order.additionalInformation)
-    const {deliveryMethod} = additionalInformation.order_metadata
+    const additionalInformation = JSON.parse(order.additionalInformation);
+    const { deliveryMethod } = additionalInformation.order_metadata;
 
     if (!email) {
       return {
@@ -20,18 +20,21 @@ module.exports = async function sendOrderConfirmation(orderId) {
     }
 
     function setDeliveryMessage() {
-      if (deliveryMethod === 'shipping') {
+      if (deliveryMethod === "shipping") {
         return `<p>
         Verzendkosten: <strong>${formatCurrency({
           amount: 8,
           currency: order.total.currency,
         })}</strong>
       </p>
-      <p>Leveradres: ${address.street} ${address.streetNumber}, ${address.postalCode} ${address.city}</p>
-      `} else if (deliveryMethod === 'pickup') {
-        return `<p>Geen verzendkosten (ophalen in winkel)</p>`
-      } else if (deliveryMethod === 'email') {
-        return `<p>Je kadobon wordt gemaild naar ${email} </p>`
+      <p>Leveradres: ${address.street} ${address.streetNumber}, ${
+          address.postalCode
+        } ${address.city}</p>
+      `;
+      } else if (deliveryMethod === "pickup") {
+        return `<p>Geen verzendkosten (ophalen in winkel)</p>`;
+      } else if (deliveryMethod === "email") {
+        return `<p>Je kadobon wordt gemaild naar ${email} </p>`;
       }
     }
 
@@ -86,10 +89,65 @@ module.exports = async function sendOrderConfirmation(orderId) {
       </mjml>
     `);
 
+    const { html: html2 } = mjml2html(`
+      <mjml>
+        <mj-body>
+        <mj-section>
+          <mj-column>
+            <mj-text>
+              <h1>Bestelgegevens</h1>
+              <p>Er werd een nieuwe bestelling geplaatst</p>
+              <p>
+                Bestelnummer: <strong>#${order.id}</strong>
+              </p>
+              <p>
+                Voornaam: <strong>${order.customer.firstName}</strong><br />
+                Naam: <strong>${order.customer.lastName}</strong><br />
+                Email: <strong>${email}</strong>
+              </p>
+              ${setDeliveryMessage()}
+              <p>
+                Totaal: <strong>${formatCurrency({
+                  amount: order.total.gross,
+                  currency: order.total.currency,
+                })}</strong>
+              </p>
+            </mj-text>
+            <mj-table>
+              <tr style="border-bottom: 1px solid #ecedee; text-align: left;">
+                <th style="padding: 0 15px 0 0;">Naam</th>
+                <th style="padding: 0 15px;">Hoeveelheid</th>
+                <th style="padding: 0 0 0 15px;">Totaal</th>
+              </tr>
+              ${order.cart.map(
+                (item) => `<tr>
+                  <td style="padding: 0 15px 0 0;">${item.name} (${
+                  item.sku
+                })</td>
+                  <td style="padding: 0 15px;">${item.quantity}</td>
+                  <td style="padding: 0 0 0 15px;">${formatCurrency({
+                    amount: item.price.gross * item.quantity,
+                    currency: item.price.currency,
+                  })}</td>
+                </tr>`
+              )}
+            </mj-table>
+          </mj-column>
+        </mj-section>
+        </mj-body>
+      </mjml>
+    `);
+
     await sendEmail({
       to: email,
       subject: "Bestelgegevens",
       html,
+    });
+
+    await sendEmail({
+      to: "info@anthonycandaele.com",
+      subject: "Nieuwe Bestelling",
+      html2,
     });
 
     return {
